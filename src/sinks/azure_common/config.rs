deckhouse/3p-vector

enhancement(datadog_archives sink): Add azure_blog_storage support
Signed-off-by: Vladimir Zhuk <vladimir.zhuk@datadoghq.com>

src/sinks/azure_common/config.rs
@@ -1,0 +1,110 @@
+use crate::buffers::Ackable;
+use crate::event::{EventFinalizers, EventStatus, Finalizable};
+use crate::sinks::{util::retries::RetryLogic, Healthcheck};
+use azure_core::prelude::*;
+use azure_core::HttpError;
+use azure_storage::blob::blob::responses::PutBlockBlobResponse;
+use azure_storage::blob::prelude::*;
+use azure_storage::core::prelude::*;
+use bytes::Bytes;
+use futures::FutureExt;
+use http::StatusCode;
+use snafu::Snafu;
+use std::sync::Arc;
+
+#[derive(Debug, Clone)]
+pub struct AzureBlobSinkRequest {
+    pub blob_data: Bytes,
+    pub blob_name: String,
+    pub content_encoding: Option<&'static str>,
+    pub content_type: &'static str,
+    pub finalizers: EventFinalizers,
+}
+
+impl Ackable for AzureBlobSinkRequest {
+    fn ack_size(&self) -> usize {
+        self.blob_data.len()
+    }
+}
+
+impl Finalizable for AzureBlobSinkRequest {
+    fn take_finalizers(&mut self) -> EventFinalizers {
+        std::mem::take(&mut self.finalizers)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct AzureBlobRetryLogic;
+
+impl RetryLogic for AzureBlobRetryLogic {
+    type Error = HttpError;
+    type Response = AzureBlobResponse;
+
+    fn is_retriable_error(&self, error: &Self::Error) -> bool {
+        match error {
+            HttpError::UnexpectedStatusCode { received, .. } => {
+                received.is_server_error() || received == &StatusCode::TOO_MANY_REQUESTS
+            }
+            _ => false,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct AzureBlobResponse {
+    pub inner: PutBlockBlobResponse,
+}
+
+impl AsRef<EventStatus> for AzureBlobResponse {
+    fn as_ref(&self) -> &EventStatus {
+        &EventStatus::Delivered
+    }
+}
+
+#[derive(Debug, Snafu)]
+pub enum HealthcheckError {
+    #[snafu(display("Invalid connection string specified"))]
+    InvalidCredentials,
+    #[snafu(display("Container: {:?} not found", container))]
+    UnknownContainer { container: String },
+    #[snafu(display("Unknown status code: {}", status))]
+    Unknown { status: StatusCode },
+}
+
+pub fn build_healthcheck(
+    container_name: String,
+    client: Arc<ContainerClient>,
+) -> crate::Result<Healthcheck> {
+    let healthcheck = async move {
+        let request = client.get_properties().execute().await;
+
+        match request {
+            Ok(_) => Ok(()),
+            Err(reason) => Err(match reason.downcast_ref::<HttpError>() {
+                Some(HttpError::UnexpectedStatusCode { received, .. }) => match *received {
+                    StatusCode::FORBIDDEN => HealthcheckError::InvalidCredentials.into(),
+                    StatusCode::NOT_FOUND => HealthcheckError::UnknownContainer {
+                        container: container_name,
+                    }
+                    .into(),
+                    status => HealthcheckError::Unknown { status }.into(),
+                },
+                _ => reason,
+            }),
+        }
+    };
+
+    Ok(healthcheck.boxed())
+}
+
+pub fn build_client(
+    connection_string: String,
+    container_name: String,
+) -> crate::Result<Arc<ContainerClient>> {
+    let client =
+        StorageAccountClient::new_connection_string(new_http_client(), connection_string.as_str())?
+            .as_storage_client()
+            .as_container_client(container_name);
+
+    Ok(client)
+}

src/sinks/azure_common/service.rs
@@ -1,0 +1,73 @@
+use crate::internal_events::azure_blob::{
+    AzureBlobErrorResponse, AzureBlobEventSent, AzureBlobHttpError,
+};
+use crate::sinks::azure_common::config::{AzureBlobResponse, AzureBlobSinkRequest};
+use azure_core::HttpError;
+use azure_storage::blob::prelude::*;
+use futures::{future::BoxFuture, TryFutureExt};
+use std::{
+    result::Result as StdResult,
+    sync::Arc,
+    task::{Context, Poll},
+};
+use tower::Service;
+use tracing_futures::Instrument;
+
+#[derive(Clone)]
+pub struct AzureBlobService {
+    pub client: Arc<ContainerClient>,
+}
+
+impl AzureBlobService {
+    pub const fn new(client: Arc<ContainerClient>) -> AzureBlobService {
+        AzureBlobService { client }
+    }
+}
+
+impl Service<AzureBlobSinkRequest> for AzureBlobService {
+    type Response = AzureBlobResponse;
+    type Error = Box<dyn std::error::Error + std::marker::Send + std::marker::Sync>;
+    type Future = BoxFuture<'static, StdResult<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<StdResult<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, request: AzureBlobSinkRequest) -> Self::Future {
+        let client = Arc::clone(&self.client).as_blob_client(request.blob_name.as_str());
+
+        Box::pin(async move {
+            let byte_size = request.blob_data.len();
+            let blob = client
+                .put_block_blob(request.blob_data)
+                .content_type(request.content_type);
+            let blob = match request.content_encoding {
+                Some(encoding) => blob.content_encoding(encoding),
+                None => blob,
+            };
+
+            let result = blob
+                .execute()
+                .inspect_err(|reason| {
+                    match reason.downcast_ref::<HttpError>() {
+                        Some(HttpError::UnexpectedStatusCode { received, .. }) => {
+                            emit!(&AzureBlobErrorResponse { code: *received })
+                        }
+                        _ => emit!(&AzureBlobHttpError {
+                            error: reason.to_string()
+                        }),
+                    };
+                })
+                .inspect_ok(|result| {
+                    emit!(&AzureBlobEventSent {
+                        request_id: result.request_id,
+                        byte_size
+                    })
+                })
+                .instrument(info_span!("request"))
+                .await;
+
+            result.map(|inner| AzureBlobResponse { inner })
+        })
+    }
+}

jknight/vector

src/sinks/azure_common/config.rs
@@ -1,0 +1,110 @@
+use crate::buffers::Ackable;
+use crate::event::{EventFinalizers, EventStatus, Finalizable};
+use crate::sinks::{util::retries::RetryLogic, Healthcheck};
+use azure_core::prelude::*;
+use azure_core::HttpError;
+use azure_storage::blob::blob::responses::PutBlockBlobResponse;
+use azure_storage::blob::prelude::*;
+use azure_storage::core::prelude::*;
+use bytes::Bytes;
+use futures::FutureExt;
+use http::StatusCode;
+use snafu::Snafu;
+use std::sync::Arc;
+
+#[derive(Debug, Clone)]
+pub struct AzureBlobRequest {
+    pub blob_data: Bytes,
+    pub blob_name: String,
+    pub content_encoding: Option<&'static str>,
+    pub content_type: &'static str,
+    pub finalizers: EventFinalizers,
+}
+
+impl Ackable for AzureBlobRequest {
+    fn ack_size(&self) -> usize {
+        self.blob_data.len()
+    }
+}
+
+impl Finalizable for AzureBlobRequest {
+    fn take_finalizers(&mut self) -> EventFinalizers {
+        std::mem::take(&mut self.finalizers)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct AzureBlobRetryLogic;
+
+impl RetryLogic for AzureBlobRetryLogic {
+    type Error = HttpError;
+    type Response = AzureBlobResponse;
+
+    fn is_retriable_error(&self, error: &Self::Error) -> bool {
+        match error {
+            HttpError::UnexpectedStatusCode { received, .. } => {
+                received.is_server_error() || received == &StatusCode::TOO_MANY_REQUESTS
+            }
+            _ => false,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct AzureBlobResponse {
+    pub inner: PutBlockBlobResponse,
+}
+
+impl AsRef<EventStatus> for AzureBlobResponse {
+    fn as_ref(&self) -> &EventStatus {
+        &EventStatus::Delivered
+    }
+}
+
+#[derive(Debug, Snafu)]
+pub enum HealthcheckError {
+    #[snafu(display("Invalid connection string specified"))]
+    InvalidCredentials,
+    #[snafu(display("Container: {:?} not found", container))]
+    UnknownContainer { container: String },
+    #[snafu(display("Unknown status code: {}", status))]
+    Unknown { status: StatusCode },
+}
+
+pub fn build_healthcheck(
+    container_name: String,
+    client: Arc<ContainerClient>,
+) -> crate::Result<Healthcheck> {
+    let healthcheck = async move {
+        let request = client.get_properties().execute().await;
+
+        match request {
+            Ok(_) => Ok(()),
+            Err(reason) => Err(match reason.downcast_ref::<HttpError>() {
+                Some(HttpError::UnexpectedStatusCode { received, .. }) => match *received {
+                    StatusCode::FORBIDDEN => HealthcheckError::InvalidCredentials.into(),
+                    StatusCode::NOT_FOUND => HealthcheckError::UnknownContainer {
+                        container: container_name,
+                    }
+                    .into(),
+                    status => HealthcheckError::Unknown { status }.into(),
+                },
+                _ => reason,
+            }),
+        }
+    };
+
+    Ok(healthcheck.boxed())
+}
+
+pub fn build_client(
+    connection_string: String,
+    container_name: String,
+) -> crate::Result<Arc<ContainerClient>> {
+    let client =
+        StorageAccountClient::new_connection_string(new_http_client(), connection_string.as_str())?
+            .as_storage_client()
+            .as_container_client(container_name);
+
+    Ok(client)
+}

src/sinks/azure_common/service.rs
@@ -1,0 +1,73 @@
+use crate::internal_events::azure_blob::{
+    AzureBlobErrorResponse, AzureBlobEventSent, AzureBlobHttpError,
+};
+use crate::sinks::azure_common::config::{AzureBlobRequest, AzureBlobResponse};
+use azure_core::HttpError;
+use azure_storage::blob::prelude::*;
+use futures::{future::BoxFuture, TryFutureExt};
+use std::{
+    result::Result as StdResult,
+    sync::Arc,
+    task::{Context, Poll},
+};
+use tower::Service;
+use tracing_futures::Instrument;
+
+#[derive(Clone)]
+pub struct AzureBlobService {
+    pub client: Arc<ContainerClient>,
+}
+
+impl AzureBlobService {
+    pub const fn new(client: Arc<ContainerClient>) -> AzureBlobService {
+        AzureBlobService { client }
+    }
+}
+
+impl Service<AzureBlobRequest> for AzureBlobService {
+    type Response = AzureBlobResponse;
+    type Error = Box<dyn std::error::Error + std::marker::Send + std::marker::Sync>;
+    type Future = BoxFuture<'static, StdResult<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<StdResult<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, request: AzureBlobRequest) -> Self::Future {
+        let client = Arc::clone(&self.client).as_blob_client(request.blob_name.as_str());
+
+        Box::pin(async move {
+            let byte_size = request.blob_data.len();
+            let blob = client
+                .put_block_blob(request.blob_data)
+                .content_type(request.content_type);
+            let blob = match request.content_encoding {
+                Some(encoding) => blob.content_encoding(encoding),
+                None => blob,
+            };
+
+            let result = blob
+                .execute()
+                .inspect_err(|reason| {
+                    match reason.downcast_ref::<HttpError>() {
+                        Some(HttpError::UnexpectedStatusCode { received, .. }) => {
+                            emit!(&AzureBlobErrorResponse { code: *received })
+                        }
+                        _ => emit!(&AzureBlobHttpError {
+                            error: reason.to_string()
+                        }),
+                    };
+                })
+                .inspect_ok(|result| {
+                    emit!(&AzureBlobEventSent {
+                        request_id: result.request_id,
+                        byte_size
+                    })
+                })
+                .instrument(info_span!("request"))
+                .await;
+
+            result.map(|inner| AzureBlobResponse { inner })
+        })
+    }
+}